131570091a188c92f212c3cae7f954648c6f2d03
kanounfoued <fouad.kanoun@legal-doctrine.com>
2021-03-08 19:44:49 +0100

    :sparkles: create a new component on ProgressStepper

---
+++ src/component/ProgressStep/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ProgressStepContainer = styled.div`
+  display: flex;
+  align-items: center;
+  transform: translate(17.5px, -110px);
+  position: absolute;
+`;
