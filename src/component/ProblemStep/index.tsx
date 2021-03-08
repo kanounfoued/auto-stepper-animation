0b323411980d28cfec4cf83b04f26006cd740558
kanounfoued <fouad.kanoun@legal-doctrine.com>
2021-03-08 13:53:39 +0100

    :sparkles: create the problem path component

---
+++ src/component/ProblemStep/index.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from 'react';
+import Stick from '../Stick';
+import Circle from '../Circle';
+import STATE_TYPE from '../../declaration_types/State';
+import { ProblemStepContainer } from './style';
+import { mockData } from './mock';
+
+export interface IPropTypes {
+  state: STATE_TYPE;
+  isProblem: boolean;
+}
+
+const ProblemStep: FC<IPropTypes> = (props) => {
+  const { state } = props;
+
+  return (
+    <ProblemStepContainer data-testid="problem-step-container">
+      {mockData.map((shape, index) => {
+        let startColor = shape.startColor;
+        let endColor = shape.endColor;
+        let active = true;
+
+        if (shape.points > state.points) {
+          startColor = '#b2bec3';
+          endColor = '#b2bec3';
+          active = false;
+        }
+
+        if (shape.type === 'circle') {
+          return <Circle key={index} circle={{ ...shape, startColor, endColor }} active={active} />;
+        } else if (shape.type === 'stick') {
+          return <Stick key={index} stick={{ ...shape, startColor, endColor }} active={active} />;
+        }
+      })}
+    </ProblemStepContainer>
+  );
+};
+
+export default ProblemStep;
---
+++ src/component/ProblemStep/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ProblemStepContainer = styled.div`
+  display: flex;
+  align-items: center;
+  transform: translate(80px, -17px);
+  position: absolute;
+`;
